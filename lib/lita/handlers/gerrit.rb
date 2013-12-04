ce01cc5cf3de984e0ebe93863ff2342252441e4d
Jonathan Amiez <jonathan.amiez@gmail.com>
2013-12-04 17:38:24 +0100

    add basic Gerrit URL generator from room

--- lib/lita/handlers/gerrit.rb
+++ lib/lita/handlers/gerrit.rb
@@ -3,6 +3,29 @@ require "lita"
 module Lita
   module Handlers
     class Gerrit < Handler
+
+      def self.default_config(config)
+        config.url = nil
+      end
+
+      # Display link to Gerrit patchset easily
+      route /gerrit\s+(\d+)/, :gerrit_url, help: { "gerrit <patchset #>" => "Displays link to gerrit patchset" }
+
+      # Simply concatenate config.url with 'id' from route regex
+      def gerrit_url(response)
+        if Lita.config.handlers.gerrit.url
+          gerrit_url = Lita.config.handlers.gerrit.url.chomp("/")
+        else
+          raise "Gerrit URL must be defined ('config.handlers.gerrit.url')"
+        end
+
+        patchset_id = response.matches.flatten.first
+        patchset_url = "#{gerrit_url}/#{patchset_id}"
+
+        response.reply("Review #{patchset_id} is at #{patchset_url}")
+      rescue Exception => e
+        response.reply("Error: #{e.message}")
+      end
     end
 
     Lita.register_handler(Gerrit)
--- lita-gerrit.gemspec
+++ lita-gerrit.gemspec
@@ -3,9 +3,9 @@ Gem::Specification.new do |spec|
   spec.version       = "0.0.1"
   spec.authors       = ["Jonathan Amiez"]
   spec.email         = ["jonathan.amiez@gmail.com"]
-  spec.description   = %q{TODO: Add a description}
-  spec.summary       = %q{TODO: Add a summary}
-  spec.homepage      = "TODO: Add a homepage"
+  spec.description   = "Notification from Gerrit hooks"
+  spec.summary       = "Display notifications when patch arre submitted, commented or merged in Gerrit"
+  spec.homepage      = "https://github.com/josqu4red/lita-gerrit"
   spec.license       = "MIT"
 
   spec.files         = `git ls-files`.split($/)
