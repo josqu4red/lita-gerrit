71ef7e20035c58420f735883d9859f8cbd2412b6
Jonathan Amiez <jonathan.amiez@gmail.com>
2014-02-24 15:34:03 +0100

    add a couple tests

--- spec/lita/handlers/gerrit_spec.rb
+++ spec/lita/handlers/gerrit_spec.rb
@@ -1,4 +1,8 @@
 require "spec_helper"
 
 describe Lita::Handlers::Gerrit, lita_handler: true do
+  it { routes("get me gerrit 123, please").to(:gerrit_url) }
+  it { doesnt_route("gerrit foo").to(:gerrit_url) }
+
+  it { routes_http(:post, "/lita/gerrit").to(:receive) }
 end
