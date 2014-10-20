5cd873cddc574f04fac6972a7b5fcbdc85b46588
Jonathan Amiez <jonathan.amiez@gmail.com>
2014-10-20 23:38:44 +0200

    move test payload to fixture file

--- spec/lita/handlers/gerrit_spec.rb
+++ spec/lita/handlers/gerrit_spec.rb
@@ -13,12 +13,9 @@ describe Lita::Handlers::Gerrit, lita_handler: true do
   it { routes("get me gerrit 123, please").to(:change_details) }
   it { doesnt_route("gerrit foo").to(:change_details) }
 
-  it { routes_http(:post, "/gerrit/hooks").to(:hook) }
-
   describe "#change_details" do
-    let(:response) do
-      double("HTTParty::Response")
-    end
+    let(:response) { double("HTTParty::Response") }
+    let(:body) { File.read("spec/fixtures/change_details.json") }
 
     before do
       allow(HTTParty).to receive(:get).and_return(response)
@@ -26,31 +23,9 @@ describe Lita::Handlers::Gerrit, lita_handler: true do
 
     it "replies with the title and URL for the issue" do
       allow(response).to receive(:code).and_return(200)
-
-      allow(response).to receive(:body).and_return(<<-JSON.chomp
-)]}'
-{
-  "kind": "gerritcodereview#change",
-  "id": "chef~master~Ib0b61ed3eebb8e22596a8401bf976949d798826a",
-  "project": "chef",
-  "branch": "master",
-  "topic": "beanstalk",
-  "change_id": "Ib0b61ed3eebb8e22596a8401bf976949d798826a",
-  "subject": "haproxy : migrate beanstalk frontend",
-  "status": "MERGED",
-  "created": "2014-03-14 12:42:15.320000000",
-  "updated": "2014-03-17 10:32:21.311000000",
-  "_sortkey": "002bcd180000096b",
-  "_number": 42,
-  "owner": {
-    "name": "John Doe"
-  }
-}
-JSON
-      )
+      allow(response).to receive(:body).and_return(body)
 
       send_command("gerrit 42")
-
       expect(replies.last).to eq("gerrit: haproxy : migrate beanstalk frontend by John Doe in chef. https://gerrit.example.com/42")
     end
 
